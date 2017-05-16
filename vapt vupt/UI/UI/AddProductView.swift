c714325e557f9e2fb0205672a82a5d599eb7f65b
Vinicius Vendramini <vinivendra@gmail.com>
2017-05-16 17:30:19 -0300

    Adding missing files.

--- vapt vupt/UI/UI/AddProductView.swift
+++ vapt vupt/UI/UI/AddProductView.swift
@@ -1,37 +1,5 @@
 import UIKit
 
-extension UIView {
-	func fillWithSubview(_ view: UIView, margins: UIEdgeInsets = .zero) {
-		view.translatesAutoresizingMaskIntoConstraints = false
-		self.topAnchor.constraint(equalTo: view.topAnchor,
-		                          constant: margins.top).isActive = true
-		self.bottomAnchor.constraint(equalTo: view.bottomAnchor,
-		                             constant: margins.bottom).isActive = true
-		self.leadingAnchor.constraint(equalTo: view.leadingAnchor,
-		                              constant: margins.left).isActive = true
-		self.trailingAnchor.constraint(equalTo: view.trailingAnchor,
-		                               constant: margins.right).isActive = true
-	}
-
-	// Set the file's owner to be the view's class
-	// Set the view's nibView outlet to be the xib's root view
-	@discardableResult
-	func loadAndFillWithNib<T: UIView>() -> T? {
-		guard let className = String(describing:
-			type(of: self)).components(separatedBy: ".").last,
-		let view = Bundle.main.loadNibNamed(className,
-		                                    owner: self,
-		                                    options: nil)?[0] as? T
-			else
-		{
-			return nil
-		}
-		self.addSubview(view)
-		self.fillWithSubview(view)
-		return view
-	}
-}
-
 class AddProductView: UIView {
 	@IBOutlet var nibView: UIView!
 
---
+++ vapt vupt/UI/UIView.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+extension UIView {
+	func fillWithSubview(_ view: UIView, margins: UIEdgeInsets = .zero) {
+		view.translatesAutoresizingMaskIntoConstraints = false
+		self.topAnchor.constraint(equalTo: view.topAnchor,
+		                          constant: margins.top).isActive = true
+		self.bottomAnchor.constraint(equalTo: view.bottomAnchor,
+		                             constant: margins.bottom).isActive = true
+		self.leadingAnchor.constraint(equalTo: view.leadingAnchor,
+		                              constant: margins.left).isActive = true
+		self.trailingAnchor.constraint(equalTo: view.trailingAnchor,
+		                               constant: margins.right).isActive = true
+	}
+
+	/// In order for this to work: set the file's owner in the xib to be the
+	/// view's class, and set the view's nibView outlet to be the xib's root
+	/// view. Call this method in every init.
+	@discardableResult
+	func loadAndFillWithNib<T: UIView>() -> T? {
+		guard let className = String(describing:
+			type(of: self)).components(separatedBy: ".").last,
+			let view = Bundle.main.loadNibNamed(className,
+			                                    owner: self,
+			                                    options: nil)?[0] as? T
+			else
+		{
+			return nil
+		}
+		self.addSubview(view)
+		self.fillWithSubview(view)
+		return view
+	}
+}
